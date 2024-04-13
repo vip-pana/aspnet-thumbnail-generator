871ebf5bd8d29b794179a2fce1b2d4a44e69c103
Pana <vip_P4na@protonmail.com>
2024-04-13 16:25:53 +0100

    version 1: created thumbnailApi, user can send an image and receive the resized 100x100 image, this using the library SixLabors.ImageSharp

---
+++ ThumbnailGenerator.API/Controllers/ThumbnailController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThumbnailGenerator.API.Services;
+
+namespace ThumbnailGenerator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThumbnailController : ControllerBase
+    {
+        private readonly IThumbnailService _thumbnailService;
+
+        public ThumbnailController(IThumbnailService thumbnailService)
+        {
+            _thumbnailService = thumbnailService;
+        }
+
+        [HttpPost("resize")]
+        public async Task<IActionResult> Resize([FromForm(Name = "Data")] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File not provided");
+
+            using (var ms = new MemoryStream())
+            {
+                await file.CopyToAsync(ms);
+                var imageData = ms.ToArray();
+                var resizedImage = _thumbnailService.ResizeImageAsync(imageData);
+                return File(resizedImage, "image/jpeg"); 
+            }
+        }
+    }
+}
---
+++ ThumbnailGenerator.API/Services/ThumbnailService.cs
@@ -0,0 +1,25 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ThumbnailGenerator.API.Services
+{
+    public interface IThumbnailService
+    {
+        byte[] ResizeImageAsync(byte[] imageData);
+    }
+
+    public class ThumbnailService : IThumbnailService
+    {
+        public byte[] ResizeImageAsync(byte[] imageData)
+        {
+            using (var image = Image.Load(imageData))
+            {
+                image.Mutate(x => x.Resize(100, 100)); // Resize to 100x100
+                using (var stream = new MemoryStream())
+                {
+                    return stream.ToArray();
+                }
+            }
+        }
+    }
+}
